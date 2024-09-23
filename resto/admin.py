de420e1d8167d23809a1b37dc2cf9b5c442297f1
Israel Shedrack <israelshedrack913@gmail.com>
2024-09-23 14:51:40 +0100

    starting build from scratch

--- resto/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User, Product, PaymentMethod, Order
-
-admin.site.register([User, PaymentMethod, Order, Product])
-
-# Register your models here.
--- resto/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RestoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'resto'
