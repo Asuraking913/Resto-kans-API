f76bca88f1ea317e2308f3e2d8af99b88c05cfdb
Israel Shedrack <israelshedrack913@gmail.com>
2024-10-01 22:23:59 +0100

    fix : new changes

--- RestoKans/settings.py
+++ RestoKans/settings.py
@@ -83,7 +83,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "allauth.account.middleware.AccountMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'RestoKans.urls'
